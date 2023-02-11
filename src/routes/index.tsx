53640d021eab2e68ce06753fe249e0f992f51f2e
baarcela <andres.arcela@bairesdev.com>
2023-02-11 11:21:32 -0400

    Adding video background

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,16 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <div
-      class="bg-cover bg-center min-h-screen"
-      style={"background-image: url(/comingSoon.png)"}
-    >
+    <div class="bg-cover bg-center min-h-screen">
+      <video
+        autoPlay
+        muted
+        loop
+        id="myVideo"
+        class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+      >
+        <source id="background-video" src="/bg_video.mp4" type="video/mp4" />
+      </video>
       {/* <img src="/public/comingSoon.png" alt="landing" class="object-cover" /> */}
     </div>
   );
